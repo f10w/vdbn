26c0ef1c25aa3a28e7a315ba5b7e0f99433cc54e
f10w <alexeyf10w@gmail.com>
2024-11-11 19:56:46 +0500

    Update initial.py
    добавлены комменты

--- initial.py
+++ initial.py
@@ -1,5 +1,6 @@
+#импортируем либу для рабты с зами
 import sqlite
-
+#делаем соединение дл бд и проверяем базу или создаем новую
 connection = sqlite3.connect('data.db')
-
+#закрываем соединение
 connection.close()
