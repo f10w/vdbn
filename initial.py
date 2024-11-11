12f2768f1e02f7923b38a9abbf0d95f0352aff16
f10w <alexeyf10w@gmail.com>
2024-11-11 19:59:22 +0500

    Update initial.py

--- initial.py
+++ initial.py
@@ -2,5 +2,7 @@
 import sqlite
 #делаем соединение дл бд и проверяем базу или создаем новую
 connection = sqlite3.connect('data.db')
+#создаём курсор базы
+cur = connection.cursor()
 #закрываем соединение
 connection.close()
